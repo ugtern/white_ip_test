982cab7d0936f20d4a71405aef70e6380f278c2b
s.v.zhiltsov <s.v.zhiltsov@newcontact.su>
2019-11-07 13:25:32 +0400

    коммит

--- index.py
+++ index.py
@@ -8,11 +8,11 @@ here = Path(__file__).resolve().parent
 
 @aiohttp_jinja2.template('index.html')
 def index_handler(request):
-    return {'name': 'Andrew'}
+    return {'world': 'word'}
 
 
 app = web.Application()
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(str(here)))
 app.router.add_get('/', index_handler)
 
-web.run_app(app, host='0.0.0.0', port=80)
+web.run_app(app, host='0.0.0.0', port=8000)
